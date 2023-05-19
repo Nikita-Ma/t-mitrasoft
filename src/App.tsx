78161bea5d27873110919d1d815330835f1bde56
Nik <nikitama1hr@gmail.com>
2023-05-19 22:56:04 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>
+                Hi there! I start develop test application!
+            </h1>
+            <br/>
+            <code>let's go!</code>
+        </div>
+    );
+}
+
+export default App;
